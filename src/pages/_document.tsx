0a2ed609a0451ecdc680754d953936d78bd6f766
Kuldeep Dhaka <kdhaka94@gmail.com>
2022-06-27 13:58:47 +0000

    update

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,16 +1,24 @@
-import { Html, Main, NextScript } from 'next/document';
-import Head from 'next/head';
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document";
 
-export default function MyDocument() {
-  return (
-    <Html lang="en" className="scroll-smooth">
-      <Head>
-        <title>KD | Portfolio</title>
-      </Head>
-      <body className="bg-white text-black antialiased dark:bg-gray-900 dark:text-white">
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+export default class CustomDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    return await Document.getInitialProps(ctx);
+  }
+  render() {
+    return (
+      <Html lang="en">
+        <Head><title>KD | Portfolio</title></Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
